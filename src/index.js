950f498cc9593246a51cd0d48f90a7713a7757c4
zbys1av <zbyslav13@gmail.com>
2023-06-10 21:20:21 +0300

    header (add changing color text)

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import './style.scss';
 
 console.log('Webpack starter with SCSS and dev server');
 
+window.addEventListener("click", test);
+
+
+function test() {
+    const selObj = window.getSelection();
+    console.log(selObj);
+    const selRange = selObj.getRangeAt(0);
+    // do stuff with the range
+  }
